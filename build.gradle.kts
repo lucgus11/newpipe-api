289c94f835fddca73aa71e15ad22d0b1bdde7261
lucgus11 <lucas.gustin@indse.be>
2026-03-19 09:01:42 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.luctube"
@@ -17,12 +18,21 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-content-negotiation")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("io.ktor:ktor-server-cors")
+    implementation("io.ktor:ktor-server-netty:2.3.7")
+    implementation("io.ktor:ktor-server-core:2.3.7")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+    implementation("io.ktor:ktor-server-cors:2.3.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("com.github.TeamNewPipe:NewPipeExtractor:v0.24.0")
     implementation("ch.qos.logback:logback-classic:1.4.14")
+}
+
+tasks {
+    shadowJar {
+        archiveBaseName.set("newpipe-api")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+        mergeServiceFiles()
+    }
 }
